91d151c438fcf241897899ff860f20ee9ece7b70
Puneet Shrivas <puneetshrivas32@gmail.com>
2021-05-27 05:52:24 +0530

    Minor updates

--- firstCaseBetaDemo-main/Public/src/app/team/team.component.ts
+++ firstCaseBetaDemo-main/Public/src/app/team/team.component.ts
@@ -15,14 +15,14 @@ export class TeamComponent implements OnInit {
         name: 'Utkarsh Agrawal',
         img: '../../assets/img/team/team-1-utkarsh.jpg',
         title: 'Co-founder & CEO',
-        mail: 'utkarshonly@gmail.com',
+        mail: 'utkarsh@firstcase.io',
         linkedin: 'https://www.linkedin.com/in/utkarsh-agrawal-13816797',
         facebook: '',
-        education: 'ISB PGP Cohort 22',
+        education: 'Indian School of Business',
         experience: [
           'Ex Associate Legal Manager ITC',
           'Ex Associate Cyril Amarchand Mangaldas',
-          'LLB, NUJS, 2018',
+          'LLB, NUJS',
           'Ex-IIT Kanpur',
         ],
       },
@@ -30,11 +30,11 @@ export class TeamComponent implements OnInit {
         name: 'Puneet Shrivas',
         img: '../../assets/img/team/team-2-puneet.jpg',
         title: 'Co-founder & CTO',
-        mail: 'puneetshrivas32@gmail.com',
+        mail: 'puneet@firstcase.io',
         linkedin: 'https://www.linkedin.com/in/puneet-shrivas/',
         facebook: 'https://www.facebook.com/puneet.shrivas.7/',
-        education: 'B.Tech, IIT Bombay, 2021',
-        experience: ['Engineering Intern, Lear Corporation'],
+        education: 'B.Tech, IIT Bombay',
+        experience: [],
       },
       {
         name: 'Tejas Ladhe',
@@ -43,7 +43,7 @@ export class TeamComponent implements OnInit {
         mail: 'tejasladhe24@gmail.com',
         linkedin: 'https://www.linkedin.com/in/tejasladhe24/',
         facebook: 'https://www.facebook.com/tejasladhe24/',
-        education: 'B.Tech + M.Tech, IIT Bombay, 2022',
+        education: 'B.Tech + M.Tech, IIT Bombay',
         experience: [],
       },
 
@@ -54,7 +54,8 @@ export class TeamComponent implements OnInit {
         mail: 'ladhepushpak@gmail.com',
         linkedin: 'https://www.linkedin.com/in/pushpak-ladhe-1a9931146/',
         facebook: 'https://www.facebook.com/pushpak26',
-        education: 'B.E., MITS, Gwalior, 2020',
+        education: 'B.E., MITS, Gwalior',
+        experience: [],
       },
       {
         name: 'Rishabh Jain',
@@ -63,8 +64,7 @@ export class TeamComponent implements OnInit {
         mail: 'rishabhjain03april@gmail.com',
         linkedin: 'https://www.linkedin.com/in/rishabh-jain-9a114512a/',
         facebook: 'https://www.facebook.com/profile.php?id=100038231587855',
-        education: 'B.Tech + M.Tech, IIT Bombay, 2024',
-
+        education: 'B.Tech + M.Tech, IIT Bombay',
         experience: [],
       },
       {
@@ -74,7 +74,7 @@ export class TeamComponent implements OnInit {
         mail: 'utkarshc611@gmail.com',
         linkedin: 'https://www.linkedin.com/in/utkarsh-chouksay-1452a3195/',
         facebook: 'https://www.facebook.com/utkarsh.chouksay/',
-        education: 'B.Tech, MITS Gwalior, 2021',
+        education: 'B.Tech, MITS Gwalior',
 
         experience: [],
       },
@@ -85,8 +85,8 @@ export class TeamComponent implements OnInit {
         mail: 'rohan21071999@gmail.com',
         linkedin: 'https://www.linkedin.com/in/rohan-singh-738580179/',
         facebook: 'https://www.facebook.com/sin.ron.775',
-        education: 'B.Tech + M.Tech, IIT Bombay, 2022',
-        experience: ['Application Engineering Intern, KLA Tencor'],
+        education: 'B.Tech + M.Tech, IIT Bombay',
+        experience: [],
       },
     ];
   }
